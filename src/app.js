c61121953779d8816febd3422a8339d4b877cd90
Aigelov <aigelov@gmail.com>
2019-11-08 16:51:22 +0600

    Import file

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import * as Factory from './2_factory';
+
 // const greeting = 'Hello World';
 // console.log(greeting);
 //
@@ -8,52 +10,3 @@
 // };
 //
 // getData('https://jsonplaceholder.typicode.com/posts');
-
-
-class SimpleMembership {
-  constructor(name) {
-    this.name = name;
-    this.cost = 50;
-  }
-}
-
-class StandardMembership {
-  constructor(name) {
-    this.name = name;
-    this.cost = 150;
-  }
-}
-
-class PremiumMembership {
-  constructor(name) {
-    this.name = name;
-    this.cost = 500;
-  }
-}
-
-class MemberFactory {
-  static list = {
-    simple: SimpleMembership,
-    standard: StandardMembership,
-    premium: PremiumMembership
-  };
-
-  create(name, type = 'simple') {
-    const Membership = MemberFactory.list[type] || MemberFactory.list.simple;
-    const member = new Membership(name);
-    member.type = type;
-    member.define = function() {
-      console.log(`${this.name} (${this.type}): ${this.cost}`)
-    };
-    return member;
-  }
-}
-
-const factory = new MemberFactory();
-const members = [
-  factory.create('Vladilen', 'simple'),
-  factory.create('Elena', 'premium'),
-  factory.create('John', 'standard'),
-];
-
-console.log(members);
