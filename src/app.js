473dcf642881cd04f824b57fce4bdaba37d183f5
Aigelov <aigelov@gmail.com>
2019-11-08 16:37:19 +0600

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,59 @@
-const greeting = 'Hello World';
-console.log(greeting);
+// const greeting = 'Hello World';
+// console.log(greeting);
+//
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+//
+// getData('https://jsonplaceholder.typicode.com/posts');
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
 
-getData('https://jsonplaceholder.typicode.com/posts');
+class SimpleMembership {
+  constructor(name) {
+    this.name = name;
+    this.cost = 50;
+  }
+}
+
+class StandardMembership {
+  constructor(name) {
+    this.name = name;
+    this.cost = 150;
+  }
+}
+
+class PremiumMembership {
+  constructor(name) {
+    this.name = name;
+    this.cost = 500;
+  }
+}
+
+class MemberFactory {
+  static list = {
+    simple: SimpleMembership,
+    standard: StandardMembership,
+    premium: PremiumMembership
+  };
+
+  create(name, type = 'simple') {
+    const Membership = MemberFactory.list[type] || MemberFactory.list.simple;
+    const member = new Membership(name);
+    member.type = type;
+    member.define = function() {
+      console.log(`${this.name} (${this.type}): ${this.cost}`)
+    };
+    return member;
+  }
+}
+
+const factory = new MemberFactory();
+const members = [
+  factory.create('Vladilen', 'simple'),
+  factory.create('Elena', 'premium'),
+  factory.create('John', 'standard'),
+];
+
+console.log(members);
--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,9 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-           presets: ['@babel/preset-env', '@babel/preset-stage-0']
+           presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-class-properties']
         }
     }]
   }
-}
+};
