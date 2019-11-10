88d462162433eacf7b254dddaddd04e4d5c96090
Aigelov <aigelov@gmail.com>
2019-11-10 23:28:05 +0600

    Pattern prototype

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import * as Constructor from './1_constructor';
 import * as Factory from './2_factory';
+import * as Prototype from './3_prototype';
 
 // const greeting = 'Hello World';
 // console.log(greeting);
